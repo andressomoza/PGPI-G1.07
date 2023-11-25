33518855d48ad899d6275f7da39f3431b065d138
gonriblu <gonriblu@gmail.com>
2023-11-25 13:20:08 +0100

    feat:listado coches

--- proyecto/cartech_web/shop/admin.py
+++ proyecto/cartech_web/shop/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Coche, Accesorio
 
 #register
 @admin.register(Category)
@@ -13,3 +13,12 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ['available', 'created', 'updated']
     list_editable = ['price', 'available']
     prepopulated_fields = {'slug': ('name',)}
+    
+@admin.register(Coche)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['marca', 'modelo', 'combustible', 'conduccion', 'consumo', 'caballos', 'precio_inicial']
+
+@admin.register(Accesorio)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['precio', 'nombre']
+
