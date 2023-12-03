864f4a7f728de70eaf3c9787a953f01f0f90389a
gonriblu <gonriblu@gmail.com>
2023-12-03 12:46:24 +0100

    pedidos

--- proyecto/cartech_web/pedidos/views.py
+++ proyecto/cartech_web/pedidos/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from .models import Pedido
 
@@ -24,10 +25,19 @@ def listar_pedidos(request):
     context = {
         'pedidos': pedidos,
         'status': status,
-
     }
-
     return render(request, 'listar_pedidos.html', context)
+
+def buscar_pedido(request):
+    if request.method == 'POST':
+        id_pedido = request.POST.get('id_pedido')
+        if id_pedido:
+            pedido = get_object_or_404(Pedido, id_pedido=id_pedido)
+            return render(request, 'detalles_pedido.html', {'pedido': pedido})
+        else:
+            return HttpResponse("Por favor, proporciona un ID de pedido válido.")
+    else:
+        return render(request, 'buscar_pedido.html')
 
 def mis_pedidos(request):
     pedidos = Pedido.objects.filter(usuario = request.user)
@@ -41,16 +51,13 @@ def mis_pedidos(request):
 def detalle_pedido(request, id):
     pedido = get_object_or_404(Pedido, id=id)
     coche = pedido.eleccion.coche
-    accesorios = []
-    precio_total = pedido.eleccion.get_precio_total
-    if pedido.eleccion.accesorios:
-        for accesorio in pedido.eleccion.accesorios:
-            accesorios.append(accesorio)
+    accesorios = list(pedido.eleccion.accesorios.all())  
+    precio_total = pedido.eleccion.get_precio_total() 
+
     context = {
         'pedido': pedido,
-        'coche':coche,
+        'coche': coche,
         'accesorios': accesorios,
-        'precio_total':precio_total
-
+        'precio_total': precio_total
     }
     return render(request, 'detalle_pedido.html', context)
