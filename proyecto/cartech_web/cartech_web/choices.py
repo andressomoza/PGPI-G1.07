970a9a6cc8cd89cacf8129781037bbf3e89ac747
rauulhernaan <raumerbas@alum.us.es>
2023-12-05 18:04:38 +0100

    feat:customuser register
    Posibilidad de registrarte sin completar algunos campos

--- proyecto/cartech_web/cartech_web/choices.py
+++ proyecto/cartech_web/cartech_web/choices.py
@@ -4,3 +4,6 @@ from django.db import models
 class MetodoPago(models.TextChoices):
         CONTRA_REEMBOLSO =  'contra_reembolso', 'Contra reembolso'
         TARJETA =  'tarjeta', 'Tarjeta'
+
+
+MetodoPago.choices_with_empty_option = [('', 'Seleccione un método de pago')] + list(MetodoPago.choices)
