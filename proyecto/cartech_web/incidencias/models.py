46f8781a5ad7fe218df44349fae2c2922c0a7ddf
gonriblu <gonriblu@gmail.com>
2023-12-11 19:18:42 +0100

    fix: incidencias

--- proyecto/cartech_web/incidencias/models.py
+++ proyecto/cartech_web/incidencias/models.py
@@ -9,10 +9,16 @@ class Incidencia(models.Model):
         ('media', 'Media'),
         ('alta', 'Alta'),
     ]
+    ESTADO = [
+        ('Sin revisar', 'Sin revisar'),
+        ('En revisión', 'En revisión'),
+        ('Revisada', 'Revisada'),
+    ]
 
     titulo = models.CharField(max_length=255)
     descripcion = models.TextField()
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE, null=True)
     urgencia = models.CharField(max_length=5, choices=DESCRIPCION_CHOICES)
+    estado = models.CharField(max_length=15, choices=ESTADO, default='Sin revisar')
 
